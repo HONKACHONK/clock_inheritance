3e0457b861b507580491eb3edd74cdac1b4e8c56
HONKACHONK <100553453+HONKACHONK@users.noreply.github.com>
2022-04-26 14:40:08 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,12 +7,12 @@ using namespace std;
 #include "travelClock.h"
 
 int main() {
-  Clock clock1(true);
-	TravelClock clock2(true, "Rome", 9);
-	TravelClock clock3(false, "Tokyo",-7)
+	vector <Clock*> clocks(3);
+	clocks[0] = new Clock(true);
+	clocks[1] = new TravelClock(true, "Rome", 9);
+	clocks[2] = new TravelClock(false, "Tokyo", -7);
 
-	cout << clock1.get_location() << " time is " << clock1.get_hours() << ":" << setw(2) << setfill('0') << clock1.get_minutes() << setfill(' ') << "\n";
-	cout << clock2.get_location() << " time is " << clock2.get_hours() << ":" << setw(2) << setfill('0') << clock2.get_minutes() << setfill(' ') << "\n";
-	cout << clock3.get_location() << " time is " << clock3.get_hours() << ":" << setw(2) << setfill('0') << clock3.get_minutes() << setfill(' ') << "\n";
-	return 0;
-  }
+	for (int i = 0, i < clocks.size(), i++) {
+		cout << clocks[i]->get_location << " time is " << clocks[i]-> << setw(2) << setfill('0') << clocks[i]->get_minutes << setfill(' ') << "\n";
+	}
+}
